244e706a7771bba23974c23a0b387e4b8cf93896
Lei Niu <lei.niu.ny@gmail.com>
2023-09-06 15:39:09 -0400

    Update lunch.json for debug server

--- pages/api/router.js
+++ pages/api/router.js
@@ -2,6 +2,6 @@ import express from 'express';
 import generateInfo from './generateInfo.js';
 
 const router = express.Router();
-router.post('/generateInfo', generateInfo);
+router.post('/generateinfo', generateInfo);
 
 export default router;
